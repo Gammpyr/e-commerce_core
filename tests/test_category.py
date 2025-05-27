55a61fcd20264e36a5345657cee0d395a9a664db
Gammpyr <malak93@yandex.ru>
2025-05-27 15:52:45 +0300

    add tests

---
+++ tests/test_category.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_Category_init(first_category, second_category):
+    assert first_category.name == 'Mobile'
+    assert first_category.description == 'Perfect mobile phones'
+    assert len(first_category.products) == 3
+
+    assert second_category.name == 'TV'
+    assert second_category.description == 'Great TV'
+    assert len(first_category.products) == 2
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 5
+    assert second_category.product_count == 5
+
---
+++ tests/test_product.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_product_init(product):
+    assert product.name == 'Lada'
+    assert product.description == 'From Russia'
+    assert product.price == 500000.00
+    assert product.quantity == 10
