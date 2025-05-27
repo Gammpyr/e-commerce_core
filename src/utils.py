bf481fe42ca6d1ee27e799c9b549f20f0f12a4c6
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-05-27 19:20:56 +0300

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ from src.product import Product
 
 
 def get_data_from_json(filename:str ='products.json') -> dict:
-    """Возвращает данные из указанного json-файла"""
+    """ Возвращает данные из указанного json-файла """
     full_path = os.path.join('./data/', filename)
     abs_path = os.path.abspath(full_path)
 
@@ -16,7 +16,7 @@ def get_data_from_json(filename:str ='products.json') -> dict:
 
 
 def create_objects_from_data(data):
-    """ Определяем классы из полученного словаря """
+    """ Определяет классы из полученного словаря """
     categories = []
     for category in data:
         products = []
@@ -27,15 +27,3 @@ def create_objects_from_data(data):
 
     return categories
 
-
-if __name__ == '__main__':
-    json_data = get_data_from_json()
-    result = create_objects_from_data(json_data)
-    for obj in result:
-        print(obj.name)
-        print(obj.description)
-        print(obj.products)
-
-        print(obj.category_count)
-        print(obj.product_count)
-        print()
