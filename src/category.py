f934f0dfae7885ee9278437734308c56e727b026
Gammpyr <malak93@yandex.ru>
2025-05-29 17:10:39 +0300

    update category.py

--- src/category.py
+++ src/category.py
@@ -11,7 +11,16 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, new_product):
+        self.__products.append(new_product)
+
+    @property
+    def products(self):
+        for product in self.__products:
+            print(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+
