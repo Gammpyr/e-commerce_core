4146cece117504e66cf40697fd348aa84b1b6b86
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-05-29 22:35:21 +0300

    update tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,32 @@
+from src.product import Product
+
+
 def test_product_init(product):
     assert product.name == "Lada"
     assert product.description == "From Russia"
     assert product.price == 500000.00
     assert product.quantity == 10
+
+
+Product("Lada", "From Russia", 500000.00, 10)
+
+
+def test_product_new_product():
+    test_dict = {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+    result = Product.new_product(test_dict)
+    assert "Samsung Galaxy S23 Ultra" == result.name
+    assert "256GB, Серый цвет" == result.description
+    assert 180000.0 == result.price
+    assert 5 == result.quantity
+
+
+def test_product_price(product):
+    product.price = -100
+    assert product.price == 500000.0
+    product.price = 600000.0
+    assert product.price == 600000.0
