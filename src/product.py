f75c1f393608ac5958e1b7ea73910f0ee810af67
Gammpyr <malak93@yandex.ru>
2025-06-03 13:50:09 +0300

    update product.py

--- src/product.py
+++ src/product.py
@@ -47,3 +47,9 @@ class Product:
                 self.__price = new_price
         else:
             self.__price = new_price
+
+    def __str__(self):
+        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        return (self.quantity * self.price) + (other.quantity * other.price)
